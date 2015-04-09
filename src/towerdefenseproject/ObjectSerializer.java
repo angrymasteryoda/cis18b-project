ad4a2e790fd0f8ea444d362e9ac954edec135612
wotozone <xoduszzong@gmail.com>
2015-04-08 17:34:12 -0700

    fixed errors

--- src/towerdefenseproject/ObjectSerializer.java
+++ src/towerdefenseproject/ObjectSerializer.java
@@ -15,6 +15,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption.*;
+import static java.nio.file.StandardOpenOption.*;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 /**
---
+++ src/towerdefenseproject/UserInfo.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package towerdefenseproject;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author minjikim
+ */
+public class UserInfo implements Serializable{
+    
+    private String userName;
+    private String password;
+    private int score;
+    
+    public UserInfo(String userName,String password, int score){
+        this.userName=userName;
+        this.password=password;
+        this.score=score;
+    }
+    
+    public String getUserName(){
+        return this.userName;
+    }
+    
+    public String getPassword(){
+        return this.password;
+    }
+    
+    public int getScore(){
+        return this.score;
+    }
+    
+}
